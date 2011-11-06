a62c727629d6846edeef8fecd4bc742106709c0f
whitexdragon <chong900208@hotmail.com>
2011-11-06 00:34:21 -0300

    Update src/drawing/Game.java

--- src/drawing/Game.java
+++ src/drawing/Game.java
@@ -2,6 +2,7 @@ package drawing;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
+import java.util.Vector;
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
@@ -10,6 +11,8 @@ import javax.imageio.*;
 public class Game extends JPanel implements Runnable {
     Thread main;
     Player player;
+    Vector enemyvector = new Vector();
+    Enemy enemy;
     
     public Game() {
         main = new Thread(this);
@@ -23,6 +26,10 @@ public class Game extends JPanel implements Runnable {
         setBackground(Color.green);
         g.setColor(Color.red);
         g.fillOval(player.getPosx(), player.getPosy(), player.getWidth(), player.getHeight());
+        for(int i = 0; i < enemyvector.size(); i++){
+            enemy = (Enemy) enemyvector.get(i);
+            g.fillOval(enemy.getPosx(), enemy.getPosy(), enemy.getWidth(), enemy.getHeight());
+        }
     }
     
     public void keyPressed (KeyEvent e) {
@@ -34,6 +41,8 @@ public class Game extends JPanel implements Runnable {
     }
     
     public void run() {
+        Enemy1 mini1 = new Enemy1(50,50,20,20,player.getPosx(),player.getPosy());
+        enemyvector.add(mini1);
         while (true) {
             try {
                 Thread.sleep(20);
@@ -41,7 +50,13 @@ public class Game extends JPanel implements Runnable {
                 System.out.println("Interrupted");
             }
             //player.setPosx(player.getPosx() + 5);
+            for(int i = 0; i < enemyvector.size(); i++){
+                enemy = (Enemy) enemyvector.get(i);
+                enemy.playerx = player.getPosx();
+                enemy.playery = player.getPosy();
+            }
             repaint();
+            
         }
     }
 }
