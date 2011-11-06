8a3673e8e96d74ec32ceb0368bfed10890462b51
Chun Ping Ng <chn22@pitt.edu>
2011-11-06 14:39:11 -0500

    shoot

--- src/drawing/Enemy1.java
+++ src/drawing/Enemy1.java
@@ -4,7 +4,8 @@ public class Enemy1 extends Enemy implements Runnable{
     
     public Enemy1(int px, int py, int w, int h, int targetx, int targety){
         super(px,py,w,h,targetx,targety);
-        delay = 20;
-        health = 10;        
+        delay = 45;
+        health = 10; 
+        missiletype = 2;
     }
 }
