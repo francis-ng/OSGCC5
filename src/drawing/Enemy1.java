32102af2b3f4953a18259d472611c4e83599e8f1
whitexdragon <chong900208@hotmail.com>
2011-11-06 00:32:37 -0300

    added data

---
+++ src/drawing/Enemy1.java
@@ -0,0 +1,10 @@
+package drawing;
+
+public class Enemy1 extends Enemy implements Runnable{
+    
+    public Enemy1(int px, int py, int w, int h, int targetx, int targety){
+        super(px,py,w,h,targetx,targety);
+        delay = 20;
+        health = 100;        
+    }
+}
