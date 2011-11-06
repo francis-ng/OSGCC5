f9c2e638fcc14c260ee7bb98c2a9d600e682e862
Francis Ng <fnha2000@gmail.com>
2011-11-06 13:05:39 -0500

    minor

--- src/drawing/Enemy1.java
+++ src/drawing/Enemy1.java
@@ -5,6 +5,6 @@ public class Enemy1 extends Enemy implements Runnable{
     public Enemy1(int px, int py, int w, int h, int targetx, int targety){
         super(px,py,w,h,targetx,targety);
         delay = 20;
-        health = 100;        
+        health = 10;        
     }
 }
